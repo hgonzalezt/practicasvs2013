425c7a654a895f04bd6907605d70a10374eebaa5
hgonzalezt <32441233+hgonzalezt@users.noreply.github.com>
2017-10-01 17:41:25 -0500

    Subir fuentes de directorios
    Este es el programa para leer directorios y archivos para procesar.

---
+++ Ejercicios_directorios/Ejercicios_directorios/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Ejercicios_directorios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_validar = new System.Windows.Forms.Button();
+            this.txt_salida = new System.Windows.Forms.TextBox();
+            this.txt_detalles = new System.Windows.Forms.TextBox();
+            this.btn_detalle_dir = new System.Windows.Forms.Button();
+            this.dtgrid_servidor = new System.Windows.Forms.DataGridView();
+            this.Servidor_ok = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.servidor_fallo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad_ok = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad_fallo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrid_servidor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_validar
+            // 
+            this.btn_validar.Location = new System.Drawing.Point(46, 38);
+            this.btn_validar.Name = "btn_validar";
+            this.btn_validar.Size = new System.Drawing.Size(109, 23);
+            this.btn_validar.TabIndex = 0;
+            this.btn_validar.Text = "Validar Directorio";
+            this.btn_validar.UseVisualStyleBackColor = true;
+            this.btn_validar.Click += new System.EventHandler(this.btn_validar_Click);
+            // 
+            // txt_salida
+            // 
+            this.txt_salida.AcceptsReturn = true;
+            this.txt_salida.Location = new System.Drawing.Point(46, 78);
+            this.txt_salida.Multiline = true;
+            this.txt_salida.Name = "txt_salida";
+            this.txt_salida.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_salida.Size = new System.Drawing.Size(204, 121);
+            this.txt_salida.TabIndex = 1;
+            // 
+            // txt_detalles
+            // 
+            this.txt_detalles.AcceptsReturn = true;
+            this.txt_detalles.Location = new System.Drawing.Point(269, 78);
+            this.txt_detalles.Multiline = true;
+            this.txt_detalles.Name = "txt_detalles";
+            this.txt_detalles.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_detalles.Size = new System.Drawing.Size(550, 121);
+            this.txt_detalles.TabIndex = 2;
+            // 
+            // btn_detalle_dir
+            // 
+            this.btn_detalle_dir.Location = new System.Drawing.Point(269, 38);
+            this.btn_detalle_dir.Name = "btn_detalle_dir";
+            this.btn_detalle_dir.Size = new System.Drawing.Size(114, 23);
+            this.btn_detalle_dir.TabIndex = 3;
+            this.btn_detalle_dir.Text = "Validar Detalle";
+            this.btn_detalle_dir.UseVisualStyleBackColor = true;
+            this.btn_detalle_dir.Click += new System.EventHandler(this.btn_detalle_dir_Click);
+            // 
+            // dtgrid_servidor
+            // 
+            this.dtgrid_servidor.AllowUserToAddRows = false;
+            this.dtgrid_servidor.AllowUserToDeleteRows = false;
+            this.dtgrid_servidor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrid_servidor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Servidor_ok,
+            this.servidor_fallo,
+            this.cantidad_ok,
+            this.cantidad_fallo});
+            this.dtgrid_servidor.Location = new System.Drawing.Point(46, 249);
+            this.dtgrid_servidor.Name = "dtgrid_servidor";
+            this.dtgrid_servidor.ReadOnly = true;
+            this.dtgrid_servidor.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dtgrid_servidor.Size = new System.Drawing.Size(443, 150);
+            this.dtgrid_servidor.TabIndex = 4;
+            // 
+            // Servidor_ok
+            // 
+            this.Servidor_ok.HeaderText = "Nombre Servidor LOG OK";
+            this.Servidor_ok.Name = "Servidor_ok";
+            this.Servidor_ok.ReadOnly = true;
+            this.Servidor_ok.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Servidor_ok.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // servidor_fallo
+            // 
+            this.servidor_fallo.HeaderText = "Nombre Servidor LOG FALLO";
+            this.servidor_fallo.Name = "servidor_fallo";
+            this.servidor_fallo.ReadOnly = true;
+            this.servidor_fallo.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.servidor_fallo.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // cantidad_ok
+            // 
+            this.cantidad_ok.HeaderText = "Cantidad OK";
+            this.cantidad_ok.Name = "cantidad_ok";
+            this.cantidad_ok.ReadOnly = true;
+            this.cantidad_ok.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.cantidad_ok.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // cantidad_fallo
+            // 
+            this.cantidad_fallo.HeaderText = "Estado FALLO";
+            this.cantidad_fallo.Name = "cantidad_fallo";
+            this.cantidad_fallo.ReadOnly = true;
+            this.cantidad_fallo.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.cantidad_fallo.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 503);
+            this.Controls.Add(this.dtgrid_servidor);
+            this.Controls.Add(this.btn_detalle_dir);
+            this.Controls.Add(this.txt_detalles);
+            this.Controls.Add(this.txt_salida);
+            this.Controls.Add(this.btn_validar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrid_servidor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_validar;
+        private System.Windows.Forms.TextBox txt_salida;
+        private System.Windows.Forms.TextBox txt_detalles;
+        private System.Windows.Forms.Button btn_detalle_dir;
+        private System.Windows.Forms.DataGridView dtgrid_servidor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Servidor_ok;
+        private System.Windows.Forms.DataGridViewTextBoxColumn servidor_fallo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidad_ok;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidad_fallo;
+    }
+}
+
---
+++ Ejercicios_directorios/Ejercicios_directorios/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ejercicios_directorios
+{
+    public partial class Form1 : Form
+    {
+        String ls_dir_archivo, ls_archivo_leido_ok, ls_archivo_leido_error, ls_fichero_leido;
+        public string[] linea_basetb;
+        public int contador_linea = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void leer_basetb()
+        {
+            linea_basetb = new string[2000];
+            
+            try
+            {
+                using (StreamReader sr = new StreamReader("E:\\Recursos\\soporte\\SentSQL.txt", false))
+                {
+                    string linea;
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        linea_basetb[contador_linea] = (linea);
+                        contador_linea++;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("El archivo no se puede leer");
+            }
+            
+        }
+
+        private void btn_validar_Click(object sender, EventArgs e)
+        {
+            
+            //falta leer el directorio basetb con el directorio de ficheros
+            txt_salida.Text = "";
+            string contenido = String.Empty;
+            string[] listaFicheros;
+            string[] listaFicheros_ok;
+            string[] listaFicheros_fallo;
+            string ls_nombre_leido;
+            string miDirectorio = @" E:\Recursos ";
+            int contador_ok = 0, contador_fallo=0;
+            int j_ok = 0, j_error = 0;
+
+            //DirectoryInfo
+            DirectoryInfo dir = new DirectoryInfo(miDirectorio);
+            FileInfo[] infoFicheros = dir.GetFiles();
+
+            listaFicheros_ok = new string[2000];
+            listaFicheros_fallo = new string[2000];
+
+            //método "GetFiles" que nos permite obtener la lista de ficheros que contiene un directorio
+            listaFicheros = Directory.GetFiles(miDirectorio);
+
+            for (int i = 0; i < listaFicheros.Length; i++)
+            {
+                ls_dir_archivo = listaFicheros[i];
+                txt_salida.Text = txt_salida.Text + ls_dir_archivo + "\r\n";
+            }
+
+            foreach (FileInfo infoUnFich in infoFicheros)
+            {
+               ls_nombre_leido= infoUnFich.Name ;
+               if (ls_nombre_leido.Contains("OK") == true)
+               {
+                   listaFicheros_ok[j_ok] = ls_nombre_leido;
+                   j_ok++;
+               }
+               else
+               {
+                   if (ls_nombre_leido.Contains("ERROR") == true)
+                   {
+                       listaFicheros_fallo[j_error] = ls_nombre_leido;
+                       j_error++;
+                   }
+               }       
+            }
+
+            // Call Array.Sort method.
+            //Array.Sort(listaFicheros_ok);
+           // Array.Sort(listaFicheros_fallo);
+            string temp;
+            for (int i = 1; i < j_error; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                    if (listaFicheros_fallo[i].CompareTo(listaFicheros_fallo[j]) < 0)
+                    {
+                        temp = listaFicheros_fallo[i];
+                        listaFicheros_fallo[i] = listaFicheros_fallo[j];
+                        listaFicheros_fallo[j] = temp;
+                    }
+            }
+
+            //leer el basetb y llevarlo a un vector, despues contains la sentencia
+            leer_basetb();
+           
+           
+            for (int i = 0; i < listaFicheros.Length; i++)
+            {
+                ls_archivo_leido_ok = listaFicheros[i];
+
+                for (int k = 0; k < j_ok; k++)
+                 {
+                    ls_fichero_leido = listaFicheros_ok[k];
+                    ls_archivo_leido_error = listaFicheros_fallo[k];
+
+                    if (ls_fichero_leido!=null)
+                    {
+                    if (ls_archivo_leido_ok.Contains(ls_fichero_leido) == true)
+                        {
+                            if (File.Exists(ls_archivo_leido_ok))
+                            {
+                                try
+                                {
+                                    using (StreamReader sr = new StreamReader(ls_archivo_leido_ok, false))
+                                    {
+                                        string line, ls_linea_basetb;
+                                       
+                                        
+                                        while ((line = sr.ReadLine()) != null)
+                                        {
+                                            for (int l = 0; l < contador_linea; l++)
+                                            {
+                                               ls_linea_basetb = linea_basetb[l];
+
+                                               if (line.Contains(ls_linea_basetb) == true)
+                                                {
+                                                    if (line.Contains("OK") == true)
+                                                    {
+                                                        txt_detalles.Text = txt_detalles.Text + line + "\r\n";
+                                                        contador_ok++;
+                                                    }
+                                                    else
+                                                        //revisar
+                                                    {
+                                                        contador_fallo++;
+                                                    }
+                                                }
+                                                
+                                            }                            
+                                        }
+                                        dtgrid_servidor.Rows.Add();
+                                        int cantidadfilas=dtgrid_servidor.Rows.Count-1;
+                                        dtgrid_servidor[0, cantidadfilas].Value = ls_fichero_leido;
+                                        dtgrid_servidor[1, cantidadfilas].Value = ls_archivo_leido_error;
+                                        dtgrid_servidor[2, cantidadfilas].Value = contador_ok;
+                                        if ( (contador_fallo > 1)  || (contador_linea - contador_ok)>0 ) 
+                                        {
+                                            dtgrid_servidor[3, cantidadfilas].Value=("FALLO");
+                                        }
+                                        
+                                        contador_ok = 0;
+                                        contador_fallo = 0;
+                                    }
+                                }
+                                catch (Exception)
+                                {
+                                     txt_detalles.Text = txt_detalles.Text + "El archivo no se puede leer";
+                                }
+                            }
+                            else
+                            {
+                                txt_detalles.Text = txt_detalles.Text + "El archivo no existe";
+                            }
+                        }
+                        else
+                        {
+                            txt_detalles.Text = txt_detalles.Text + "El archivo no tiene uno similar";
+                        }
+                    }
+                }
+            }               
+        }
+
+        // la clase DirectoryInfo permite obtener información sobre fechas de creación, modificación y acceso, y,
+        //de forma análoga, FileInfo nos permite conseguir información similar sobre un fichero.
+        private void btn_detalle_dir_Click(object sender, EventArgs e)
+        {
+            string miDirectorio = @" e:\Documentos 2015 ";
+            DirectoryInfo dir = new DirectoryInfo(miDirectorio);
+            FileInfo[] infoFicheros = dir.GetFiles();
+            foreach (FileInfo infoUnFich in infoFicheros)
+            {
+                   //Console.WriteLine("{0}, de tamaño {1}, creado {2}",
+                   // infoUnFich.Name,
+                   // infoUnFich.Length,
+                   // infoUnFich.CreationTime);
+                   txt_detalles.Text = txt_detalles.Text + infoUnFich.Name + "  " + infoUnFich.Length + "   " + infoUnFich.CreationTime + "\r\n";
+            }
+        }
+    }
+}
